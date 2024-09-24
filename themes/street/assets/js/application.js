1fbc796c1cbf39eab629b77f65af1e9dc9a5f2c1
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-09-24 11:00:33 +0200

    feat: anim on canvas images + hover effect

--- themes/street/assets/js/application.js
+++ themes/street/assets/js/application.js
@@ -48,11 +48,121 @@ window.onload = () => {
 
   lenis.on('scroll', ScrollTrigger.update);
 
+
+  // Canvas Anim
+
+  const canvasWidth = 375;
+  const canvasHeight = (canvasWidth / 16) * 9;
+  const targets = document.querySelectorAll('.canvas-container');
+
+  targets.forEach(async (target, index) => {
+
+    let app = new PIXI.Application({
+        width: canvasWidth,
+        height: canvasHeight
+    });
+
+    target.appendChild(app.view);
+
+    const sprite = createSprite(index);
+
+    sprite.width = canvasWidth;
+    sprite.height = canvasHeight;
+
+    const {oldFilmFilter, shockWaveFilter} = createFilters();
+    sprite.filters = [oldFilmFilter, shockWaveFilter];
+    app.stage.addChild(sprite);
+
+    const minSeed = 0.2;
+    const maxSeed = 1;
+    let seedChangeInterval = 10;
+    let frameCounter = 0;
+    let isHovered = false;
+
+    target.addEventListener('mouseenter', () => {
+      shockWaveFilter.amplitude = 10;
+      shockWaveFilter.wavelength = 450;
+      shockWaveFilter.brightness = 1.0
+      shockWaveFilter.radius = -1;
+      shockWaveFilter.speed = 500;
+      isHovered = true;
+    });
+
+    target.addEventListener('mouseleave', () => {
+      resetShockwaveFilter(shockWaveFilter)
+      isHovered = false;
+      oldFilmFilter.sepia = 0.4;
+    });
+
+    app.ticker.add((delta) => {
+        frameCounter++;
+
+        if (frameCounter % seedChangeInterval === 0) {
+            oldFilmFilter.seed = minSeed + Math.random() * (maxSeed - minSeed);
+        }
+
+        if (isHovered) {
+            shockWaveFilter.time += delta * 0.02;
+            oldFilmFilter.sepia = 0;
+            oldFilmFilter.noiseSize = 0;
+            if (shockWaveFilter.time > 5) {
+              shockWaveFilter.time = 0;
+            }
+        }
+    });
+
+});
+
 };
 
+// Focus
 
 function toggleFocus() {
   const focus = document.querySelector(".focus__area");
 
   focus.classList.toggle('active');
+}
+
+
+// Canvas Anim
+
+function createSprite(index) {
+  let imagePath = `/images/uploads/project_${index + 1}.webp`;
+  return PIXI.Sprite.from(imagePath);
+}
+
+function resetShockwaveFilter(shockWaveFilter) {
+  shockWaveFilter.amplitude = 0;
+  shockWaveFilter.wavelength = 0;
+  shockWaveFilter.brightness = 0;
+  shockWaveFilter.radius = 0;
+  shockWaveFilter.speed = 0;
+  shockWaveFilter.time = 0;
+}
+
+
+function createFilters() {
+  const oldFilmFilter = new PIXI.filters.OldFilmFilter();
+  oldFilmFilter.noiseSize = 2;
+  oldFilmFilter.noise = 0;
+  oldFilmFilter.sepia = 0.4;
+
+  const shockWaveFilter = new PIXI.filters.ShockwaveFilter();
+  resetShockwaveFilter(shockWaveFilter)
+  return { oldFilmFilter, shockWaveFilter };
+}
+
+function setupHoverEffect(target, shockWaveFilter, oldFilmFilter) {
+  target.addEventListener('mouseenter', () => {
+      shockWaveFilter.amplitude = 10;
+      shockWaveFilter.wavelength = 450;
+      shockWaveFilter.brightness = 1.0;
+      shockWaveFilter.radius = -1;
+      shockWaveFilter.speed = 500;
+  });
+
+  target.addEventListener('mouseleave', () => {
+      resetShockwaveFilter(shockWaveFilter);
+      oldFilmFilter.sepia = 0.4;
+  });
 }
